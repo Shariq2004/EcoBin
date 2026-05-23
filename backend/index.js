f08ab2d8875dae54a5163989a5d4a1db50d9eac5
Shariq Ahmad <shariqahmad303@gmail.com>
2026-05-23 19:39:51 +0530

    Updated CORS with admin URL

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 //socket setup
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://ecobin-platform.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://ecobin-platform.vercel.app", "https://ecobin-admin-platform.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -58,7 +58,7 @@ app.set("io", io);
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://ecobin-platform.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://ecobin-platform.vercel.app", "https://ecobin-admin-platform.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
